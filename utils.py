6e615755ec1fccec162ed55ccdc522a4541563b1
Álvaro Ezquerro Sastre <110617994+AlvaroEzq@users.noreply.github.com>
2026-09-11 11:16:56 +0200

    Fix bug when getting last run number from google sheet (#17)
    * get only int values of run number column of the google sheet
    
    * fix print message

--- utils.py
+++ utils.py
@@ -84,7 +84,13 @@ def get_last_run_number_from_google_sheet(worksheet_number=WORKSHEET_NUMBER):
         sheet = client.open(GOOGLE_SHEET_NAME)
         page = sheet.get_worksheet(worksheet_number) # starts from 0
         column_values = page.col_values(1)  # 1 refers to column A (run number)
-        run_numbers = [val for val in column_values if val]
+        run_numbers_col = [val for val in column_values if val]
+        run_numbers = []
+        for r in run_numbers_col:
+            try:
+                run_numbers.append(int(r))
+            except ValueError:
+                pass
         print(f"Last run number from Google Sheet: {run_numbers[-1]}")
         return run_numbers[-1]
     except Exception as e:
